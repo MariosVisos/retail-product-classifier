62ce9b947a72a9e9f1ca1273cc0976ba7889c9e1
MariosVisos <visos.marios@gmail.com>
2020-03-21 19:58:37 +0100

    Replace redux-logger with redux-devtools-extension and react-native-debugger

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 function configureStore() {
-  const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+  const store = createStore(
+    rootReducer,
+    applyMiddleware(thunk),
+    // composeWithDevTools(applyMiddleware(thunk)),
+  );
   return store;
 }
 
