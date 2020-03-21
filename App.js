3638b2eec521ac99806491a6cf6099ccb9e3c092
MariosVisos <visos.marios@gmail.com>
2020-03-21 10:03:17 +0100

    Add and configure eslint and prettier

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import { , Text, View } from 'react-native';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Retail Product Classifier</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
