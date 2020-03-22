6807398077826a041b72064b9eeaa5a8d50cebcf
MariosVisos <visos.marios@gmail.com>
2020-03-22 21:00:18 +0100

    Install react-navigation

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import Main from './src/Main';
 import configureStore from './src/store/configureStore';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   const store = configureStore();
   return (
     <Provider store={store}>
-      <Main />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Main} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
