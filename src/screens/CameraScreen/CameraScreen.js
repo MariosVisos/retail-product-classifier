2bfb829d86b23bfb034eb38578fe2c796812c5a2
MariosVisos <visos.marios@gmail.com>
2020-03-27 16:25:14 +0100

    Install expo camera

--- src/screens/CameraScreen/CameraScreen.js
+++ src/screens/CameraScreen/CameraScreen.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
 import { Button } from 'react-native-elements';
 import { Entypo } from '@expo/vector-icons';
+import { Camera } from 'expo-camera';
 import styles from './CameraScreenStyles';
 
 const CameraScreen = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const type = Camera.Constants.Type.back;
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Camera screen</Text>
-      <Button icon={<Entypo name="camera" size={32} />} raised type="outline" />
+      <Camera style={styles.camera} type={type} />
+      <View style={styles.textContainer}>
+        <Text>Camera screen</Text>
+        {/* 
+          <Button
+            icon={<Entypo name="camera" size={32} />}
+            raised
+            type="outline"
+          /> */}
+      </View>
     </View>
   );
 };
--- src/screens/CameraScreen/CameraScreenStyles.js
+++ src/screens/CameraScreen/CameraScreenStyles.js
@@ -1,7 +1,21 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  container: { alignItems: 'center', flex: 1, justifyContent: 'space-around' },
+  camera: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    // position: 'relative',
+  },
+  textContainer: {
+    alignItems: 'center',
+    borderColor: 'red',
+    borderWidth: 1,
+    flex: 1,
+    justifyContent: 'space-around',
+    position: 'absolute',
+  },
 });
 
 export default styles;
